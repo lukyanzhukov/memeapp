4d1297cdd5bde5084e51f6c77fc1173c74a86891
Анатолий Спитченко <a.spitchenko@cft.ru>
2020-04-04 18:19:44 +0700

    Добавил вебсокет в основной модуль

--- server/src/main/kotlin/ru/memebattle/Application.kt
+++ server/src/main/kotlin/ru/memebattle/Application.kt
@@ -10,6 +10,7 @@ import io.ktor.features.StatusPages
 import io.ktor.gson.gson
 import io.ktor.routing.Routing
 import io.ktor.server.cio.EngineMain
+import io.ktor.websocket.WebSockets
 import org.kodein.di.generic.instance
 import org.kodein.di.ktor.KodeinFeature
 import org.kodein.di.ktor.kodein
@@ -18,6 +19,7 @@ import ru.memebattle.auth.JwtAuth
 import ru.memebattle.di.KodeinBuilder
 import ru.memebattle.exception.ErrorHandler
 import ru.memebattle.route.RoutingV1
+import java.time.Duration
 
 fun main(args: Array<String>) {
     EngineMain.main(args)
@@ -37,6 +39,13 @@ fun Application.module() {
 
     install(KodeinFeature) {
         KodeinBuilder(environment).setup(this)
+    }
+
+    install(WebSockets) {
+        pingPeriod = Duration.ofSeconds(60) // Disabled (null) by default
+        timeout = Duration.ofSeconds(15)
+        maxFrameSize = Long.MAX_VALUE // Disabled (max value). The connection will be closed if surpassed this length.
+        masking = false
     }
 
     install(Authentication) {
