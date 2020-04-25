8c104842dc34a6deba8c0f5a801fdf185befba5c
Lukyan <l.zhukov@movavi.com>
2020-04-26 00:11:28 +0700

    MB-24: Добавил возможность выбора разлчиных режимов игры.

--- client/src/main/java/ru/memebattle/feature/GameFragment.kt
+++ client/src/main/java/ru/memebattle/feature/GameFragment.kt
@@ -8,8 +8,24 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import kotlinx.android.synthetic.main.fragment_game.*
 import ru.memebattle.R
+import ru.memebattle.common.GameMode
 
 class GameFragment : Fragment() {
+
+    private val gameModesClickListener = View.OnClickListener {
+        val bundle = Bundle()
+        when (it.id) {
+            R.id.classic_game_mode_btn -> bundle.putSerializable("GameMode", GameMode.CLASSIC)
+            R.id.senior_game_mode_btn -> bundle.putSerializable("GameMode", GameMode.SENIOR)
+            R.id.english_game_mode_btn -> bundle.putSerializable("GameMode", GameMode.ENGLISH)
+            R.id.it_game_mode_btn -> bundle.putSerializable("GameMode", GameMode.IT)
+            R.id.science_game_mode_btn -> bundle.putSerializable("GameMode", GameMode.SCIENCE)
+            R.id.work_game_mode_btn -> bundle.putSerializable("GameMode", GameMode.WORK)
+            R.id.study_game_mode_btn -> bundle.putSerializable("GameMode", GameMode.STUDY)
+        }
+        Navigation.findNavController(requireActivity(), R.id.host_global)
+            .navigate(R.id.action_mainFragment_action_to_memebattleFragment, bundle)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -21,9 +37,13 @@ class GameFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        memeClassic.setOnClickListener {
-            Navigation.findNavController(requireActivity(), R.id.host_global)
-                .navigate(R.id.action_mainFragment_action_to_memebattleFragment)
-        }
+        classic_game_mode_btn.setOnClickListener(gameModesClickListener)
+        senior_game_mode_btn.setOnClickListener(gameModesClickListener)
+        english_game_mode_btn.setOnClickListener(gameModesClickListener)
+        it_game_mode_btn.setOnClickListener(gameModesClickListener)
+        science_game_mode_btn.setOnClickListener(gameModesClickListener)
+        work_game_mode_btn.setOnClickListener(gameModesClickListener)
+        study_game_mode_btn.setOnClickListener(gameModesClickListener)
+
     }
 }
