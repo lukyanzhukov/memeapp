57692c4d227066e076d97e6a3cb3e6fb3358bd09
Lukyan <l.zhukov@movavi.com>
2020-05-05 21:56:15 +0700

    MB-56: Редизайн отображения диалогов.

--- client/src/main/java/ru/memebattle/core/utils/DialogUtils.kt
+++ client/src/main/java/ru/memebattle/core/utils/DialogUtils.kt
@@ -1,20 +1,14 @@
 package ru.memebattle.core.utils
 
-import android.content.Intent
 import android.graphics.Outline
-import android.graphics.drawable.InsetDrawable
-import android.net.Uri
-import android.provider.Settings
 import android.view.View
 import android.view.ViewOutlineProvider
-import androidx.annotation.DimenRes
-import androidx.appcompat.app.AlertDialog
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import kotlinx.android.synthetic.main.dialog_first_win.view.*
 import kotlinx.android.synthetic.main.dialog_game_onboarding.view.*
-import kotlinx.android.synthetic.main.dialog_permission_request_block.view.ok_button
 import kotlinx.android.synthetic.main.dialog_game_onboarding.view.onboarding_banner_view
+import kotlinx.android.synthetic.main.dialog_permission_request_block.view.ok_button
 import ru.memebattle.R
 
 
@@ -22,44 +16,25 @@ data class GameOnboardingDialogListener(val onCloseDialog: () -> Unit)
 
 data class FirstWinDialogListener(val onCloseDialog: () -> Unit, val onAuthClick: () -> Unit)
 
-fun Fragment.createDialog(
-    dialogView: View,
-    @DimenRes marginsRes: Int = R.dimen.alert_margins
-): AlertDialog {
-    val dialog = AlertDialog.Builder(context!!)
-        .setView(dialogView)
-        .create()
-    val bg = ContextCompat.getDrawable(requireContext(), R.drawable.bg_alert)
-    val margins = resources.getDimensionPixelOffset(marginsRes)
-    dialog.window?.setBackgroundDrawable(InsetDrawable(bg, margins))
-    return dialog
-}
-
-fun Fragment.openPermissionRequestBlockDialog() {
-    log("openPermissionRequestBlockDialog")
-    val layoutId = R.layout.dialog_permission_request_block
-    val dialogView = layoutInflater.inflate(layoutId, null, false)
-    val dialog = createDialog(dialogView)
-    dialog.setCancelable(false)
-    dialogView.ok_button.setOnClickListener {
-        val intent = Intent(
-            Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
-            Uri.fromParts("package", requireActivity().packageName, null)
-        )
-        requireActivity().startActivity(intent)
-        dialog.dismiss()
-    }
-    dialog.show()
-}
-
 fun Fragment.openGameOnboardingDialog(listener: GameOnboardingDialogListener) {
     val layoutId = R.layout.dialog_game_onboarding
     val dialogView = layoutInflater.inflate(layoutId, null, false)
-    val dialog = createDialog(dialogView)
-
+    val dialog = BottomSheetDialog(requireContext())
+    dialog.setContentView(dialogView)
+    dialog.setOnShowListener {
+        val dialog = it as BottomSheetDialog
+        val bottomSheet = dialog.findViewById<View>(R.id.design_bottom_sheet)
+        bottomSheet?.let { sheet ->
+            dialog.behavior.peekHeight = sheet.height
+            sheet.parent.parent.requestLayout()
+        }
+    }
+    dialogView.close_onboarding_dialog_btn.setOnClickListener {
+        dialog.dismiss()
+    }
     val provider = object : ViewOutlineProvider() {
         override fun getOutline(view: View, outline: Outline) {
-            outline.setRoundRect(0, 0, view.width, view.height, 16F)
+            outline.setRoundRect(0, 0, view.width, view.height, 12F)
         }
     }
 
@@ -77,11 +52,11 @@ fun Fragment.openFirstWinDialog(listener: FirstWinDialogListener) {
 
     val layoutId = R.layout.dialog_first_win
     val dialogView = layoutInflater.inflate(layoutId, null, false)
-    val dialog = createDialog(dialogView)
-
+    val dialog = BottomSheetDialog(requireContext())
+    dialog.setContentView(dialogView)
     val provider = object : ViewOutlineProvider() {
         override fun getOutline(view: View, outline: Outline) {
-            outline.setRoundRect(0, 0, view.width, view.height, 16F)
+            outline.setRoundRect(0, 0, view.width, view.height, 12F)
         }
     }
 
